2016db870674457fd945bc5811899991e47a232c
songwie <songwei@mogoroom.com>
2017-06-11 18:10:35 +0800

    mgs

--- src/main/java/com/songwie/RootApplication.java
+++ src/main/java/com/songwie/RootApplication.java
@@ -2,12 +2,19 @@ package com.songwie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.context.event.ContextClosedEvent;
- 
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionTrackingMode;
+import java.util.Collections;
+
 @SpringBootApplication
 @EnableSpringConfigured
 @EnableAspectJAutoProxy
@@ -24,4 +31,13 @@ public class RootApplication {
             }
         });
     }
+	@Bean
+	public ServletContextInitializer servletContextInitializer1() {
+		return new ServletContextInitializer() {
+			@Override
+			public void onStartup(ServletContext servletContext) throws ServletException {
+				servletContext.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE) );
+			}
+		};
+	}
 }
